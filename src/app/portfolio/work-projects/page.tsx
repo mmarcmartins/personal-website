20cd86bb3854d237aa9c50f75315ae3a52f83203
Marcelo Martins <inv.mmartins@ifood3rd.com>
2023-04-28 17:53:35 -0300

    fix pnpm

--- src/app/portfolio/work-projects/page.tsx
+++
@@ -1,8 +0,0 @@
-export const revalidate = 1800 // revalidate every 30 minutes
-export const metadata = {
-  title: 'Work projects',
-}
-
-export default async function WorkProjects() {
-  return(<p></p>);
-}
